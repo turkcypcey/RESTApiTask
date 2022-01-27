2c84773e1a4f2dda1d09dc2f6f218296abeb6e56
Allan Callaghan <calaldees@gmail.com>
2022-01-27 16:37:42 +0000

    Stubs for automated tests

---
+++ test_server/conftest.py
@@ -0,0 +1,35 @@
+import pytest
+
+import os
+import time
+import socket
+from urllib.parse import urlparse
+
+
+def is_port_open(hostname, port):
+    return socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect_ex((hostname, port)) == 0
+
+
+@pytest.fixture(scope="session")
+def ENDPOINT():
+    return os.environ.get('URI_SERVER', 'http://localhost:8000')
+
+
+@pytest.fixture(scope="session", autouse=True)
+def wait_for_service(ENDPOINT):
+    """
+    Before starting tests
+    Wait for service to become available
+    """
+    _endpoint = urlparse(ENDPOINT)
+    for attempt in range(10):
+        try:
+            if is_port_open(_endpoint.hostname, _endpoint.port):
+                return
+        except Exception as ex:
+            pass
+        time.sleep(1)
+    raise Exception(f"{ENDPOINT} port is not active")  # TODO: This does not seem to stop execution of tests?
+    #request.addfinalizer(finalizer_function)
+
+
