440cc2522e2ebca9d39f610452938f04fc43d1f0
turkcypcey <cc835@canterbury.ac.uk>
2022-07-11 15:44:31 +0000

    DELETE done and tested

--- app/server.js
+++ app/server.js
@@ -4,8 +4,9 @@ const port = 8000
 
 app.use(express.json());
 
-const attendees = [
-    
+attendees = [
+  {"id": 123, "name": "test", "notes": "some notes"},
+  {"id": 124, "name": "test", "notes": "some notes"}  
 ]
 
 app.get('/', (req, res) => {
@@ -19,6 +20,11 @@ app.get('/attendees/', (req, res) => [
 app.post('/attendee/', (req, res) => {
   attendees.push(req.body)
   res.status(201).json(req.body)
+})
+app.delete('/attendee/:id', (req,res) =>{
+  console.log("to be deleted", req.params.id)
+  attendees = attendees.filter((i)=> i.id !=req.params.id)
+  res.status(204).json({})
 })
 
 app.listen(port, () => {
