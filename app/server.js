a7e180d540e3a62d530f76fb78223698e2a81b98
turkcypcey <cc835@canterbury.ac.uk>
2022-07-11 11:56:36 +0000

    client.html on loads up attendees

--- app/server.js
+++ app/server.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+const attendees = [
+    {"foo": "bar676"},
+]
+
 app.get('/', (req, res) => {
   res.sendFile('client.html', {root: __dirname})
 })
+
+app.get('/attendees/', (req, res) => [
+    res.json(attendees),
+])
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
