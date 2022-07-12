bc277c8fc20c41b0c3662db4866bac0f6d1f95a5
turkcypcey <cc835@canterbury.ac.uk>
2022-07-12 12:14:21 +0000

    started writeup.md

--- app/server.js
+++ app/server.js
@@ -26,6 +26,7 @@ app.post('/attendee', (req, res) => {
   attendees.push(req.body)
   res.status(201).json(req.body)
 })
+
 app.delete('/attendee/:id', (req,res) =>{
   attendees = attendees.filter((i)=> i.id !=req.params.id)
   res.status(204).json({})
